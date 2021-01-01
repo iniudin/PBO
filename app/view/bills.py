82b7d52aeae4bd27c99cbe892ec2714d9a5ffa70
Shohibun Najam <71695550+Shohibun@users.noreply.github.com>
2021-01-01 09:54:09 +0700

    yaa mungkin bisa seperti itu

--- app/view/bills.py
+++ app/view/bills.py
@@ -1,5 +1,48 @@
 from app.models.bills import BillsModel
+from app.functions import headline, print_table 
 
 
 class BillsView(BillsModel):
-    pass
+    def main_menu(self):
+        text_menu = (
+            ["1", "Daftar pembayaran"],
+            ["2", "Cash"],
+            ["3", "Debbit card"]
+        )
+        print_table("Menu pembayaran", ["No", "Deskripsi"], text_menu)
+        choice = int(input("> "))
+        if choice == 0:
+            return 
+        elif choice == 1:
+            Cash()
+        elif choice == 3:
+            DebbitCard()
+        else:
+            print("Perintah tidak ditemukan")
+
+    def Cash(self):
+        print("Jika Anda memilih pembayaran secara langsung")
+        print("Silahkan menuju ke tempat kasir untuk melakukan pembayaran")
+        print("Terima Kasih")
+        Riwayat()
+
+    def DebbitCard(self):
+        print("Jika Anda memilih pembayaran secara Debbit card")
+        print("Anda harus memasukkan PIN Anda")
+        print("Jika sudah selesai maka akan ada kalimat: ")
+        print("Transaksi Anda sukses")
+        Riwayat()
+
+    def Riwayat(self):
+        #Intinya disinituh aku pengen kalau milih pembyaran cash akan ada fungsi riwayat yang menuju ke pembyaran cash
+        #Sedangkan kalau lewat debbit card maka juga akan menuju ke riwayat debbit card 
+        if Cash():
+            print("Riwayat Pembayaran")
+        else:
+            print("Riwayat Pembayaran")
+        self.BackToMenu()
+
+    @property
+    def BackToMenu(self):
+        input("Tekan Enter untuk kembali ke Menu")
+        self.main_menu
